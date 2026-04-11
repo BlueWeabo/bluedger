f7b402ae06688ff458c2c12712d05c1c1f2449ff
BlueWeabo <ilia.iliev@tuta.com>
2026-04-11 22:47:03 +0300

    textedit

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,16 @@
+use std::ptr::null;
+
+use egui::{Id, Rangef};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
     label: String,
+    last_loaded_file_contents: String,
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
+    //##[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
 }
 
@@ -14,19 +19,15 @@ impl Default for TemplateApp {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
+            last_loaded_file_contents: "test".to_owned(),
             value: 2.7,
         }
     }
 }
 
 impl TemplateApp {
-    /// Called once before the first frame.
+    // Called on startup/open
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
         } else {
@@ -45,10 +46,9 @@ impl eframe::App for TemplateApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
+        let TEXT_EDITOR_ID : Id = Id::new("text_editor");
 
-        egui::Panel::top("top_panel").show_inside(ui, |ui| {
-            // The top panel is often a good place for a menu bar:
-
+        egui::Panel::top("header").show_inside(ui, |ui|{
             egui::MenuBar::new().ui(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
@@ -63,33 +63,31 @@ impl eframe::App for TemplateApp {
 
                 egui::widgets::global_theme_preference_buttons(ui);
             });
+            ui.vertical_centered(|ui| {
+                ui.heading("bluedger");
+            });
+        });
+
+        egui::Panel::bottom("footer").show_inside(ui, |ui| {
+            ui.vertical_centered(|ui| {
+                egui::warn_if_debug_build(ui);
+            });
+            ui.horizontal(|ui| {
+                powered_by_egui_and_eframe(ui);
+                ui.add(egui::github_link_file!(
+                    "https://github.com/BlueWeabo/bluedger/blob/main/",
+                    "Source code."
+                ));
+            });
+        });
+
+        let window_width = ui.ctx().input(|i| {i.content_rect()}).width();
+        egui::Panel::left("text_editor_panel").size_range(Rangef::new(window_width * 0.2, window_width * 0.5)).show_inside(ui, |ui|{
+            ui.add_sized(ui.available_size(), egui::TextEdit::multiline(&mut self.last_loaded_file_contents).id(TEXT_EDITOR_ID));
         });
 
         egui::CentralPanel::default().show_inside(ui, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
         });
     }
 }
